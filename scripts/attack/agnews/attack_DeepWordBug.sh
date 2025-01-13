944094e34bc9c3d9977d15684ce127d1f3ba71ef
Yakuru-43 <yacine.jf@gmail.com>
2025-01-13 12:28:42 +0100

    restauring default config

--- scripts/attack/agnews/attack_DeepWordBug.sh
+++ scripts/attack/agnews/attack_DeepWordBug.sh
@@ -2,13 +2,13 @@
 
 # Sample script to run main.py with specific command-line arguments.
 # You can modify the argument values as needed.
-
+# CUDA_VISIBLE_DEVICES=1 \
 python main.py \
     --mode attack \
     --method DeepWordBug \
     --config config.yml \
     --batchsize 16 \
     --sample-size 100 \
-    --precision half \
+    --precision full \
     --dataset agnews \
     --mask_word "<mask>"
