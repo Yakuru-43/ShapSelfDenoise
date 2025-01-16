beaa7241dcd378da94960f6e6221d78528e72251
Yakuru-43 <yacine.jf@gmail.com>
2025-01-16 14:57:09 +0100

    Attacks dor sst2

---
+++ scripts/attack/sst2/attack_TextBugger.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Sample script to run main.py with specific command-line arguments.
+# You can modify the argument values as needed.
+# CUDA_VISIBLE_DEVICES=0 \
+
+python main.py \
+    --mode attack \
+    --method TextBugger \
+    --config config.yml \
+    --batchsize 16 \
+    --sample-size 100 \
+    --precision half \
+    --dataset sst2 \
+    --mask_word "<mask>"
+
+# # Check for uncommitted changes
+# if ! git diff-index --quiet HEAD --; then
+
+#   # Stage all changes
+#   git add -A
+
+#   # Commit changes with a timestamp
+#   commit_message="Automated commit on $(date '+%Y-%m-%d %H:%M:%S')"
+#   git commit -m "$commit_message"
+
+#   # Push to the master branch
+#   git push origin master
+
+#   echo "Changes pushed to GitHub successfully."
+
+# else
+#   echo "No changes to commit."
+# fi
